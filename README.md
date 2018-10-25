5145349d6714b8911f189de9a605744fa28bde24
wangjiafenghw <wang_jiafeng@yahoo.com>
2018-10-25 20:02:48 +0800

    先提交，以为我要睡觉了

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # code-word
 符号立体文字注释
+
+> 灵感来源于[@jamiebuilds](https://github.com/jamiebuilds)的开源代码
---
+++ getcode.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('word.js'),
+    output: fs.createWriteStream('output.js')
+});
+
+var index = '';
+const word = new Object();
+rl.on('line', (line) => {
+
+    if (line >= 'A' && line <= 'Z') {
+        index = line;
+        word[index] = [];
+    } else {
+        var l = [];
+        for (let i = 0; i < line.length; i++) {
+            switch (line[i]) {
+                case ' ':
+                    l.push(0)
+                    break;
+                case '█':
+                    l.push(1)
+                    break;
+                case '═':
+                    l.push(2)
+                    break;
+                case '╗':
+                    l.push(3)
+                    break;
+                case '╔':
+                    l.push(4)
+                    break;
+                case '║':
+                    l.push(5)
+                    break;
+                case '╝':
+                    l.push(6)
+                    break;
+                case '╚':
+                    l.push(7)
+                    break;
+                case '#':
+                    
+                    break;
+                default:
+                    console.log('意外情况', line[i])
+                    break;
+            }
+        }
+        word[index].push(l)
+    }
+})
+
+setTimeout(() => {
+    fs.writeFile("output.js", JSON.stringify(word), ()=>{
+        console.log('ok')
+    })
+}, 10000);  //这里有点儿low,rl.on有结束回调吗…… =||
+
+
+
+
+
