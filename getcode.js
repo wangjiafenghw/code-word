3ece96afadcacb5e03ffd14a91f5c2164ec3dc6c
wangjiafenghw <wang_jiafeng@yahoo.com>
2018-10-26 08:03:38 +0800

    事实这个高逼格的注释

--- getcode.js
+++ getcode.js
@@ -5,7 +5,10 @@ const rl = readline.createInterface({
     input: fs.createReadStream('word.js'),
     output: fs.createWriteStream('output.js')
 });
-
+/**
+ * TODO 得出字母图案的二位矩阵
+ * !rl.on到底有没有回调，啊~~
+ */
 var index = '';
 const word = new Object();
 rl.on('line', (line) => {
@@ -16,7 +19,7 @@ rl.on('line', (line) => {
     } else {
         var l = [];
         for (let i = 0; i < line.length; i++) {
-            switch (line[i]) {
+            switch (line[i]) {   
                 case ' ':
                     l.push(0)
                     break;
